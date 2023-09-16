52153a4a22393fdbce193009306a20c601485e13
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-09-16 22:28:06 +0200

    init config

---
+++ config.h
@@ -0,0 +1,57 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 250  // delay between switching from tap to held a key
+
+// #define OLED_BRIGHTNESS 0
+
+#undef RGBLED_NUM
+#undef RGBLIGHT_ANIMATIONS
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
+#undef RGBLED_NUM
+#undef RGBLIGHT_LIMIT_VAL
+#undef RGBLIGHT_HUE_STEP
+#undef RGBLIGHT_SAT_STEP
+#undef RGBLIGHT_VAL_STEP
+
+// Underglow
+/*
+#undef RGBLED_NUM
+#define RGBLED_NUM 14    // Number of LEDs
+#define RGBLIGHT_SLEEP
+*/
---
+++ keymap.c
@@ -0,0 +1,193 @@
+#include QMK_KEYBOARD_H
+
+
+/* Empty layout
+* ,-----------------------------------------.                    ,-----------------------------------------.
+* |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+* |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+* |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+* |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+* |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
+* |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+* |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+* `-----------------------------------------/       /     \      \-----------------------------------------'
+*                   |      |      |      | /       /       \      \  |      |      |      |
+*                   |      |      |      |/       /         \      \ |      |      |      |
+*                   `----------------------------'           '------''--------------------'
+*/
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT(
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, 
+		KC_CAPS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RALT, 
+		MO(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_UP, MO(1), 
+		MO(2), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TAB, KC_BSPC, KC_N, KC_M, KC_QUOT, KC_LEFT, KC_DOWN, KC_RGHT, 
+		KC_LCTL, KC_LALT, KC_LGUI, LSFT_T(KC_SPC), SC_SENT, MO(1), KC_DOT, KC_COMM
+	),
+	
+	[1] = LAYOUT(
+		KC_TRNS, KC_BRID, KC_BRIU, KC_NO, KC_NO, KC_NO, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, 
+		KC_TRNS, KC_GRV, KC_TILD, KC_HASH, KC_LBRC, KC_RBRC, KC_EXLM, KC_PPLS, KC_PMNS, KC_PEQL, KC_ASTR, KC_PERC, 
+		KC_TRNS, KC_AT, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_QUES, KC_SLSH, KC_PIPE, KC_BSLS, KC_LT, KC_GT, 
+		KC_TRNS, KC_NO, KC_NO, KC_DLR, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_NO, KC_UNDS, KC_DQUO, KC_NO, KC_NO, KC_NO, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_COLN, KC_SCLN
+	),
+
+	[2] = LAYOUT(
+		KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, 
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_HOME, KC_PGUP, 
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_END, KC_PGDN, 
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO
+	)
+};
+
+#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+};
+#endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
+
+
+// KEY OVERIDES
+// https://docs.qmk.fm/#/feature_key_overrides + https://docs.qmk.fm/#/keycodes
+const key_override_t esc_key_override      = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, KC_NO);
+const key_override_t n1_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_1, KC_NO);
+const key_override_t n2_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_NO);
+const key_override_t n3_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_3, KC_NO);
+const key_override_t n4_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_4, KC_NO);
+const key_override_t n5_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_5, KC_NO);
+const key_override_t n6_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_6, KC_NO);
+const key_override_t n7_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_7, KC_NO);
+const key_override_t n8_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_8, KC_NO);
+const key_override_t n9_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_9, KC_NO);
+const key_override_t n0_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_0, KC_NO);
+const key_override_t dot_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_NO);
+const key_override_t coma_key_override       = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_NO);
+const key_override_t question_key_override    = ko_make_basic(MOD_MASK_SHIFT, KC_QUES, KC_NO);
+const key_override_t slash_key_override    = ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, KC_NO);
+const key_override_t quote_key_override   = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_NO);
+
+const key_override_t kc_grv_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_GRV, KC_NO);
+const key_override_t kc_tild_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_TILD, KC_NO);
+const key_override_t kc_hash_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_HASH, KC_NO);
+const key_override_t kc_lbrc_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, KC_NO);
+const key_override_t kc_rbrc_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, KC_NO);
+const key_override_t kc_exlm_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_EXLM, KC_NO);
+const key_override_t kc_ppls_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_PPLS, KC_NO);
+const key_override_t kc_pmns_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_PMNS, KC_NO);
+const key_override_t kc_peql_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_PEQL, KC_NO);
+const key_override_t kc_astr_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_ASTR, KC_NO);
+const key_override_t kc_perc_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_PERC, KC_NO);
+const key_override_t kc_at_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_AT, KC_NO);
+const key_override_t kc_circ_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_CIRC, KC_NO);
+const key_override_t kc_ampr_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_AMPR, KC_NO);
+const key_override_t kc_lprn_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_NO);
+const key_override_t kc_rprn_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_NO);
+const key_override_t kc_pipe_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_PIPE, KC_NO);
+const key_override_t kc_bsls_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_BSLS, KC_NO);
+const key_override_t kc_lt_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_LT, KC_NO);
+const key_override_t kc_gt_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_GT, KC_NO);
+const key_override_t kc_dlr_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_DLR, KC_NO);
+const key_override_t kc_lcbr_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_LCBR, KC_NO);
+const key_override_t kc_rcbr_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_RCBR, KC_NO);
+const key_override_t kc_unds_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_UNDS, KC_NO);
+const key_override_t kc_dquo_key_override   =  ko_make_basic(MOD_MASK_SHIFT, KC_DQUO, KC_NO);
+
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+  &esc_key_override,
+	&n1_key_override,
+	&n2_key_override,
+	&n3_key_override,
+	&n4_key_override,
+	&n5_key_override,
+	&n6_key_override,
+	&n7_key_override,
+	&n8_key_override,
+	&n9_key_override,
+	&n0_key_override,
+	&dot_key_override,
+	&coma_key_override,
+	&question_key_override,
+	&slash_key_override,
+	&quote_key_override,
+	&kc_grv_key_override,
+	&kc_tild_key_override,
+	&kc_hash_key_override,
+	&kc_lbrc_key_override,
+	&kc_rbrc_key_override,
+	&kc_exlm_key_override,
+	&kc_ppls_key_override,
+	&kc_pmns_key_override,
+	&kc_peql_key_override,
+	&kc_astr_key_override,
+	&kc_perc_key_override,
+	&kc_at_key_override,
+	&kc_circ_key_override,
+	&kc_ampr_key_override,
+	&kc_lprn_key_override,
+	&kc_rprn_key_override,
+	&kc_pipe_key_override,
+	&kc_bsls_key_override,
+	&kc_lt_key_override,
+	&kc_gt_key_override,
+	&kc_dlr_key_override,
+	&kc_lcbr_key_override,
+	&kc_rcbr_key_override,
+	&kc_unds_key_override,
+	&kc_dquo_key_override,
+  NULL // Null terminate the array of overrides!
+};
+
+
+
+
+
+
+//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master())
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  return rotation;
+}
+
+// When you add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+bool oled_task_user(void) {
+  if (is_keyboard_master()) {
+    // If you want to change the display of OLED, you need to change here
+    oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keylog(), false);
+    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    //oled_write_ln(read_host_led_state(), false);
+    //oled_write_ln(read_timelog(), false);
+  } else {
+    oled_write(read_logo(), false);
+  }
+    return false;
+}
+#endif // OLED_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef OLED_ENABLE
+    set_keylog(keycode, record);
+#endif
+    // set_timelog();
+  }
+  return true;
+}
