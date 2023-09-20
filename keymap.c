3773f8deb08967f541863d0851c8aef128875857
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-09-20 22:00:44 +0200

    display caps lock status + add some macros on layer 2

--- keymap.c
+++ keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[2] = LAYOUT(
 		KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, 
-		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_HOME, KC_PGUP, 
-		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_END, KC_PGDN, 
-		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_Z), SGUI(KC_Z), KC_PSCR, KC_HOME, KC_NO, 
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_SLSH), KC_NO, KC_NO, LGUI(KC_PPLS), KC_PGUP, 
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_PMNS), KC_PGDN, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO
 	)
 };
@@ -148,46 +148,55 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
 
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  return rotation;
-}
-
-// When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
 bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
-    return false;
+  // Set cursor position
+  oled_set_cursor(0, 1);
+
+  // Caps lock status
+  led_t led_state = host_keyboard_led_state();
+  oled_write_P(led_state.caps_lock ? PSTR( "CAPS LOCK") : PSTR("         "), false);
+
+  return false;
 }
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  return OLED_ROTATION_270;
+}
+
+// // When you add source files to SRC in rules.mk, you can use functions.
+// const char *read_layer_state(void);
+// const char *read_logo(void);
+// void set_keylog(uint16_t keycode, keyrecord_t *record);
+// const char *read_keylog(void);
+// const char *read_keylogs(void);
+
+// // const char *read_mode_icon(bool swap);
+// // const char *read_host_led_state(void);
+// // void set_timelog(void);
+// // const char *read_timelog(void);
+
+// bool oled_task_user(void) {
+//   if (is_keyboard_master()) {
+//     // If you want to change the display of OLED, you need to change here
+//     oled_write_ln(read_layer_state(), false);
+//     oled_write_ln(read_keylog(), false);
+//     oled_write_ln(read_keylogs(), false);
+//     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+//     //oled_write_ln(read_host_led_state(), false);
+//     //oled_write_ln(read_timelog(), false);
+//   } else {
+//     oled_write(read_logo(), false);
+//   }
+//     return false;
+// }
 #endif // OLED_ENABLE
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-  return true;
-}
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   if (record->event.pressed) {
+// #ifdef OLED_ENABLE
+//     set_keylog(keycode, record);
+// #endif
+//     // set_timelog();
+//   }
+//   return true;
+// }
--- rules.mk
+++ rules.mk
@@ -9,7 +9,8 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = no            # OLED display
+OLED_ENABLE = yes            # OLED display
+LTO_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
